e2cf110486b53f1b62a464e68e6f67ec79d86d8b
CaiZeHua <273362054@qq.com>
2019-06-29 15:58:35 +0800

    备忘录模式

---
+++ MementoPattern(备忘录模式)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MementoPattern_备忘录模式_
+{
+    /// <summary>
+    /// 备忘录模式：在不破坏封装性的前提下，捕获一个对象的内部状态，
+    /// 并在该对象之外保存这个状态。这样以后就可以将该对象恢复到原先保存的状态。
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
